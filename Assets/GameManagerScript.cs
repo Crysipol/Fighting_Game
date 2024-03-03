d2f57e654479baa346a9bbafd11d1b6bd2b49ddf
Kevin Paute <Kevin.Paute2@gmail.com>
2024-03-03 14:11:05 -0500

    GameManager Script

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public int lastPlayerAttackDamage;
+    public int playerHP;
+    public int enemyHP;
+    public TextMeshProUGUI informationText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHP = 1000;
+        enemyHP = 1000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        informationText.text = "Player HP: " + playerHP + "\nEnemy HP: " + enemyHP;
     }
 }
